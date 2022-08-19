0cd49e9eb4379921047be734983f9f89ee1ad1f4
mfatecha <mfatecha@sodep.com.py>
2022-08-18 22:15:42 -0400

    Inicializa app

---
+++ app-ios/app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_iosApp.swift
+//  app-ios
+//
+//  Created by monica fatecha on 2022-08-17.
+//
+
+import SwiftUI
+
+@main
+struct app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
