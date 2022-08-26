09b18caf8668cc997284a421fca7a5ea9db0e903
mfatecha <mfatecha@sodep.com.py>
2022-08-26 12:00:52 -0400

    Agrega pantalla login

--- app-ios/ContentView.swift
+++ app-ios/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Color.green.ignoresSafeArea()
+                Circle().scale(1.7).foregroundColor(.white)
+                VStack {
+                    Text("Bienvenido").font(.largeTitle).bold()
+                    TextField("Username o correo", text: $username) .padding().frame(width: 300, height: 50).background(Color.black.opacity(0.05)).cornerRadius(10)
+                    TextField("Password", text: $password).padding().frame(width: 300, height: 50).background(Color.black.opacity(0.05)).cornerRadius(10)
+                    Button("Ingresar") {
+                        //Accion de login
+                    }.foregroundColor(.white).frame(width: 300, height: 50).background(Color.mint).cornerRadius(10)
+                    
+                }
+            }
+            .navigationBarHidden(true)
+        }
     }
 }
 
