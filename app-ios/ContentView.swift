10e245aade391acb09f4d1299e066ddadcf66903
mfatecha <mfatecha@sodep.com.py>
2022-09-19 00:10:05 -0400

    Login con google mail

--- app-ios/ContentView.swift
+++ app-ios/ContentView.swift
@@ -20,18 +20,18 @@ struct ContentView: View {
                 Color.blue.ignoresSafeArea()
                 Circle().scale(1.7).foregroundColor(.white)
                 VStack {
-                    Image("logo1").frame(width: 400, height: 110)
-                    Text("Bienvenido").font(.largeTitle).bold()
-                    TextField("Username o correo",text:$username).keyboardType(.emailAddress).padding().frame(width: 300, height: 50).background(Color.black.opacity(0.05)).cornerRadius(10).foregroundColor(.blue)
-                    TextField("Password",text:$password).padding().frame(width: 300, height: 50).background(Color.black.opacity(0.05)).cornerRadius(10).foregroundColor(.blue)
-                    Button("Ingresar"){
-                        validarCredenciales()
-                    }.foregroundColor(.white).frame(width: 300, height:50).background(Color.blue).cornerRadius(10)
-                    Text("Crear Cuenta").font(.system(size: 12, weight: .light, design: .serif))
-                        .italic().underline().frame(height:20)
-                    Text("Olvide mi contraseña").font(.system(size: 12, weight: .light, design: .serif))
-                        .italic().underline()
-
+                   Text("Bienvenido").font(.largeTitle).bold()
+                    Button(action: {}) {
+                        HStack(alignment: .center, spacing: 5.0) {
+                            Image("googleMail")
+                                .padding(.leading, 10.0)
+                            Text("Ingresar con Google")
+                                .foregroundColor(.black)
+                                .padding(.all, 10.0)
+                        }
+                    }
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
                 }
             }
             .navigationBarHidden(true)
@@ -39,19 +39,7 @@ struct ContentView: View {
         }
     }
     
-    func validarCredenciales(){
-        FirebaseAuth.Auth.auth().signIn(withEmail: username, password: password,completion: {(result,error) in
-            if error != nil{
-                print("Error al iniciar sesion")
-            }else
-            {
-              print("Inicio sesion exitoso")
-                
-            }
-        })
-        
-   
-    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
